1292b1937f773c73993be0c6ad200150c8b65eb1
stendy b. sakur <32978797+sakurtek@users.noreply.github.com>
2021-08-03 09:26:24 +0800

    Update index.php
    menambah komnetar pada link css boostrap dan jquery dan popper dengan menambahkan verfi yang ada

--- index.php
+++ index.php
@@ -15,6 +15,8 @@ getErrorLogin();
   	<script src="plugin/bootstrap-4.5.3-dist/js/bootstrap.min.js"></script>
 
 <!-- w3school online 
+ ini dapat digunakan jika anda online namun anda bisa menggunakan cara 
+yang diatas sehingga proses offline dan online bisa sama
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
